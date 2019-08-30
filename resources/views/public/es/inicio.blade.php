f7c1d83e8b4d858358dd3023660d86f63c2bf631
SheryLanado <42298615+SheryLanado@users.noreply.github.com>
2019-08-30 17:36:43 -0500

    ccx
    xcv

--- resources/views/public/es/inicio.blade.php
+++ resources/views/public/es/inicio.blade.php
@@ -23,7 +23,7 @@
             </li>
           </ul>
         </div>
-        <!-- slider info-->
+        <!-- slider info--
         <div class="slider-info-wrap clearfix">
           <div class="slider-info-content">
             @foreach($data as $item)
@@ -46,7 +46,7 @@
 
           </div>
         </div>
-        <!-- ! slider-info-->
+        -- ! slider-info-->
       </div>
 
       <section class="page-section pb-0">
@@ -61,6 +61,123 @@
             <!--<div class="col-md-12"><img src="/public/pic/promo-1.png" data-at2x="pic/promo-1@2x.png" alt class="mt-md-0 mt-minus-70"></div>-->
           </div>
         </div>
+        <div class="row blog-col">
+              <!-- Blog Post-->
+              <div class="col-md-4 col-sm-6 col-xs-6 mb-30">
+                <!-- Blog item-->
+                <div class="blog-item clearfix border boxed">
+                  <!-- Blog Image-->
+                  <div class="blog-media"><a href="blog-single.html">
+                      <div class="pic"><img src="pic/blog/270x270/1@2x.jpg" data-at2x="pic/blog/270x270/1@2x.jpg" alt=""></div></a></div>
+                  <!-- blog body-->
+                  <div class="blog-item-body clearfix">
+                    <!-- title--><a href="blog-single.html">
+                      <h6 class="blog-title">Sed semper lacus et </h6></a>
+                    <div class="blog-item-data">Mon, 03-23-2016</div>
+                    <!-- Text Intro-->
+                    <p>Etiam maximus molestie accumsan. Sed metus sapien, fermentum nec lorem ac, tempor gravida arcu.</p><a href="blog-single.html" class="blog-button">Read more</a>
+                  </div>
+                </div>
+                <!-- ! Blog item-->
+              </div>
+              <!-- ! Blog Post-->
+              <!-- Blog Post-->
+              <div class="col-md-4 col-sm-6 col-xs-6 mb-30">
+                <!-- Blog item-->
+                <div class="blog-item clearfix border boxed">
+                  <!-- Blog Image-->
+                  <div class="blog-media"><a href="blog-single.html">
+                      <div class="pic"><img src="pic/blog/270x270/2@2x.jpg" data-at2x="pic/blog/270x270/2@2x.jpg" alt=""></div></a></div>
+                  <!-- blog body-->
+                  <div class="blog-item-body clearfix">
+                    <!-- title--><a href="blog-single.html">
+                      <h6 class="blog-title">Aenean nec urna </h6></a>
+                    <div class="blog-item-data">Mon, 03-23-2016</div>
+                    <!-- Text Intro-->
+                    <p>Nullam efficitur lorem eu felis rutrum elementum. Vestibulum feugiat massa id ante scelerisque blandit.</p><a href="blog-single.html" class="blog-button">Read more</a>
+                  </div>
+                </div>
+                <!-- ! Blog item-->
+              </div>
+              <!-- ! Blog Post-->
+              <!-- Blog Post-->
+              <div class="col-md-4 col-sm-6 col-xs-6 mb-30">
+                <!-- Blog item-->
+                <div class="blog-item clearfix border boxed">
+                  <!-- Blog Image-->
+                  <div class="blog-media"><a href="blog-single.html">
+                      <div class="pic"><img src="pic/blog/270x270/3@2x.jpg" data-at2x="pic/blog/270x270/3@2x.jpg" alt=""></div></a></div>
+                  <!-- blog body-->
+                  <div class="blog-item-body clearfix">
+                    <!-- title--><a href="blog-single.html">
+                      <h6 class="blog-title">Etiam sit amet </h6></a>
+                    <div class="blog-item-data">Mon, 03-23-2016</div>
+                    <!-- Text Intro-->
+                    <p>urabitur rhoncus sem et eros pharetra euismod. Ut commodo sem magna, in congue ex vestibulum ut commodo sem magna, in congue ex vestibulum ut.</p><a href="blog-single.html" class="blog-button">Read more</a>
+                  </div>
+                </div>
+                <!-- ! Blog item-->
+              </div>
+              <!-- ! Blog Post-->
+              <!-- Blog Post-->
+              <div class="col-md-4 col-sm-6 col-xs-6 mb-30">
+                <!-- Blog item-->
+                <div class="blog-item clearfix border boxed">
+                  <!-- Blog Image-->
+                  <div class="blog-media"><a href="blog-single.html">
+                      <div class="pic"><img src="pic/blog/270x270/4@2x.jpg" data-at2x="pic/blog/270x270/4@2x.jpg" alt=""></div></a></div>
+                  <!-- blog body-->
+                  <div class="blog-item-body clearfix">
+                    <!-- title--><a href="blog-single.html">
+                      <h6 class="blog-title">Integer venenatis leo </h6></a>
+                    <div class="blog-item-data">Mon, 03-23-2016</div>
+                    <!-- Text Intro-->
+                    <p>Cras tellus orci, ultricies at ornare rhoncus, vehicula ac nibh. Vivamus consectetur eget diam ac mollis.</p><a href="blog-single.html" class="blog-button">Read more</a>
+                  </div>
+                </div>
+                <!-- ! Blog item-->
+              </div>
+              <!-- ! Blog Post-->
+              <!-- Blog Post-->
+              <div class="col-md-4 col-sm-6 col-xs-6 mb-30">
+                <!-- Blog item-->
+                <div class="blog-item clearfix border boxed">
+                  <!-- Blog Image-->
+                  <div class="blog-media"><a href="blog-single.html">
+                      <div class="pic"><img src="pic/blog/270x270/5@2x.jpg" data-at2x="pic/blog/270x270/5@2x.jpg" alt=""></div></a></div>
+                  <!-- blog body-->
+                  <div class="blog-item-body clearfix">
+                    <!-- title--><a href="blog-single.html">
+                      <h6 class="blog-title">Nulla eget purus </h6></a>
+                    <div class="blog-item-data">Mon, 03-23-2016</div>
+                    <!-- Text Intro-->
+                    <p>Ut tincidunt sapien consectetur elit mollis tincidunt. Integer vel ex nec quam gravida posuere vulputate a diam.</p><a href="blog-single.html" class="blog-button">Read more</a>
+                  </div>
+                </div>
+                <!-- ! Blog item-->
+              </div>
+              <!-- ! Blog Post-->
+              <!-- Blog Post-->
+              <div class="col-md-4 col-sm-6 col-xs-6">
+                <!-- Blog item-->
+                <div class="blog-item clearfix border boxed">
+                  <!-- Blog Image-->
+                  <div class="blog-media"><a href="blog-single.html">
+                      <div class="pic"><img src="pic/blog/270x270/6@2x.jpg" data-at2x="pic/blog/270x270/6@2x.jpg" alt=""></div></a></div>
+                  <!-- blog body-->
+                  <div class="blog-item-body clearfix">
+                    <!-- title--><a href="blog-single.html">
+                      <h6 class="blog-title">Maecenas id ligula sed</h6></a>
+                    <div class="blog-item-data">Mon, 03-23-2016</div>
+                    <!-- Text Intro-->
+                    <p>Nam elementum leo ex, nec lacinia ligula malesuada nec. Phasellus bibendum tempor leo, a accumsan justo molestie at.</p><a href="blog-single.html" class="blog-button">Read more</a>
+                  </div>
+                </div>
+                <!-- ! Blog item-->
+              </div>
+              <!-- ! Blog Post-->
+            </div>
+        <!--
         <div class="features-tours-full-width">
             <div class="features-tours-wrap clearfix">
           
@@ -86,123 +203,10 @@
            
             @endforeach
           </div>
-        </div>
+        </div> -->
       </section>
       <!-- ! page section-->
-      <!-- counter section -->
-      <section class="small-section">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-2 col-xs-6 mb-md-30">
-              <div class="counter-block"><i class="counter-icon flaticon-suntour-world"></i>
-                <div class="counter-name-wrap">
-                  <div data-count="345" class="counter">0</div>
-                  <div class="counter-name">Tours</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-2 col-xs-6 mb-md-30">
-              <div class="counter-block with-divider"><i class="counter-icon flaticon-suntour-fireworks"></i>
-                <div class="counter-name-wrap">
-                  <div data-count="438" class="counter">0</div>
-                  <div class="counter-name">Holidays</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-2 col-xs-6 mb-md-30">
-              <div class="counter-block with-divider"><i class="counter-icon flaticon-suntour-hotel"></i>
-                <div class="counter-name-wrap">
-                  <div data-count="526" class="counter">0</div>
-                  <div class="counter-name">Hotels</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-2 col-xs-6 mb-md-30">
-              <div class="counter-block with-divider"><i class="counter-icon flaticon-suntour-ship"></i>
-                <div class="counter-name-wrap">
-                  <div data-count="169" class="counter">0</div>
-                  <div class="counter-name">Cruises</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-2 col-xs-6">
-              <div class="counter-block with-divider"><i class="counter-icon flaticon-suntour-airplane"></i>
-                <div class="counter-name-wrap">
-                  <div data-count="293" class="counter">0</div>
-                  <div class="counter-name">Flights</div>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-2 col-xs-6">
-              <div class="counter-block with-divider"><i class="counter-icon flaticon-suntour-car"></i>
-                <div class="counter-name-wrap">
-                  <div data-count="675" class="counter">0</div>
-                  <div class="counter-name">Cars</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <!-- ! counter section-->
-      <!-- page section parallax-->
-      <section class="small-section cws_prlx_section bg-gray-40"><img src="public/pic/parallax-1.jpg" alt class="cws_prlx_layer">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-5">
-              <h2 class="title-section-top alt">About</h2>
-              <h2 class="title-section alt mb-20"><span>Sun</span>Tour</h2>
-              <p class="color-white">Vestibulum tincidunt venenatis scelerisque. Proin quis enim lacinia, vehicula massa et, mollis urna. Proin nibh mauris, blandit vitae convallis at, tincidunt vel tellus. Praesent posuere nec lectus non cursus. Sed commodo odio et ipsum sagittis tincidunt non eget felis.</p>
-              <div class="cws_divider short mb-30 mt-30"></div>
-              <h3 class="font-5 color-white font-medium">Andrew McDonald</h3>
-            </div>
-            <div class="col-md-7">
-              <div class="embed-responsive embed-responsive-16by9">
-                <iframe src="https://www.youtube.com/embed/yib7tvIrL6k" class="embed-responsive-item"></iframe>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-      <!-- ! page section parallax-->
-      <!-- recomended section-->
-      <section class="small-section bg-gray">
-        <div class="container">
-          <div class="row">
-            <div class="col-md-8">
-              <!--<h6 class="title-section-top font-4">Top rated</h6>-->
-              <h2 class="title-section"><span>TOURS</span> RECOMENDADOS</h2>
-              <div class="cws_divider mb-25 mt-5"></div>
-              <!--<p>Maecenas commodo odio ut vulputate cursus. Integer in egestas lectus. Nam volutpat feugiat mi vitae mollis. Aenean tristique dolor bibendum mi scelerisque ultrices non at lorem.</p>-->
-            </div>
-            <div class="col-md-4"><i class="flaticon-suntour-hotel title-icon"></i></div>
-          </div>
-          <div class="row">
-            <!-- Recomended item-->
-            @foreach($data as $item)
-            <div class="col-md-6">
-              <div class="recom-item">
-                <div class="recom-media"><a href="{{route('detalletour',['tour'=>$item->slug])}}">
-                    <div class="pic"><img src="{{$item->img}}" data-at2x="{{$item->img}}" alt style="width:770px; height:240px;"></div></a>
-                  <div class="location"><i class="flaticon-suntour-map"></i> Istanbul, Turkey</div>
-                </div>
-                <!-- Recomended Content-->
-                <div class="recom-item-body"><a href="{{route('detalletour',['tour'=>$item->slug])}}">
-                    <h6 class="blog-title">{{$item->nombretour}}</h6></a>
-                  <div class="stars stars-4"></div>
-                  <div class="recom-price"><span class="font-4">$90</span> per night</div>
-                  <p class="mb-30">    </p><a href="{{route('detalletour',['tour'=>$item->slug])}}" class="cws-button small alt">Leer más</a>
-                  <div class="action font-2">20%</div>
-                </div>
-                <!-- Recomended Image-->
-              </div>
-            </div>
-            @endforeach
- 
-          </div>
-        </div>
-      </section>
-      <!-- ! recomended section-->
+      
       <!-- testimonials section-->
       <section class="small-section cws_prlx_section bg-blue-40"><img src="pic/parallax-2.jpg" alt class="cws_prlx_layer">
         <div class="container">
--- resources/views/public/es/layouts/navbar.blade.php
+++ resources/views/public/es/layouts/navbar.blade.php
@@ -1,57 +1,9 @@
  
  <!-- header page-->
     <header>
-      <!-- site top panel-->
-      <div class="site-top-panel">
-        <div class="container p-relative">
-          <div class="row">
-            <div class="col-md-6 col-sm-7">
-              <!-- lang select wrapper-->
-              <div class="top-left-wrap font-3">
-                <div class="mail-top"><a href="mailto:reservas@machupicchugolden.com"> <i class="flaticon-suntour-email"></i>prueba@tours.com</a></div><span>/</span>
-                <div class="tel-top"><a href="tel:(723)-700-1183"> <i class="flaticon-suntour-phone"></i>0051 084 584 272 </a></div>
-              </div>
-              <!-- ! lang select wrapper-->
-            </div>
-            <div class="col-md-6 col-sm-5 text-right">
-              <div class="top-right-wrap">
-                
-              
-              <div class="lang-wrap dropdown">
-                  <div>
-                    <ul>
-                      <li><a href="#" class="lang-sel icl-en"> <span class="glyphicon glyphicon-user"></span>  Iniciar sesión <i class="fa fa-angle-down"></i></a>
-                        <ul>
-                          <li><a href="/register">Crear cuenta</a></li>
-                          <li> 
-                              <div class="top-login"><a href="#">Iniciar sesión</a></div>
-                          </li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-
-                <div class="lang-wrap dropdown">
-                  <div>
-                    <ul>
-                      <li><a href="#" class="lang-sel icl-en">Language <i class="fa fa-angle-down"></i></a>
-                        <ul>
-                          <li><a href="#">English</a></li>
-                          <li> <a href="#">Español</a></li>
-                        </ul>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- ! site top panel-->
+      
       <!-- Navigation panel-->
-      <nav class="main-nav js-stick">
+      <nav class="main-nav transparent stick-fixed">
         <div class="full-wrapper relative clearfix container">
           <!-- Logo ( * your text or image into link tag *)-->
           <!-- <div class="nav-logo-wrap local-scroll"><a href="index.html" class="logo"><img src="/public/img/logo.png" data-at2x="img/logo@2x.png" style="height: 20px;"></a></div> -->
@@ -81,37 +33,22 @@
 
               <li><a href="page-contact.html">TESTIMONIOS</a></li>
                <li class="slash">/</li>
-
-              <li><a href="page-contact.html">CONDICIONES</a></li>
-               <li class="slash">/</li>
              
               <li><a href="/blog">BLOG</a></li>
                <li class="slash">/</li>
               <!-- End Item With Sub-->
-              <li><a href="/contacto">CONTACTO </a>
+              <li><a href="/contacto">CONTACTO </a></li>
+              <li class="slash">/</li>
+              <!-- End Item With Sub-->
+              <li><a href="#" class="mn-has-sub">LANGUAJE<i class="fa fa-angle-down button_open"></i></a>
                 <!-- Sub-->
-              
+                <ul class="mn-sub">
+                  <li><a href="{{route('paquete',['idioma'=> 'es','categoria'=>'aventura'])}}">ENGLISH</a></li>
+                  <li><a href="{{route('paquete',['idioma'=> 'es','categoria'=>'mistico'])}}">ESPAÑOL</a></li>
+                </ul>
                 <!-- End Sub-->
               </li>
-              <!-- End Item With Sub-->
-            
               
-              <li class="search"><a href="#" class="mn-has-sub">Buscar tours</a>
-                <ul class="search-sub">
-                  <li>
-                    <div class="container">
-                      <div class="mn-wrap">
-                        <form method="post" class="form">
-                          <div class="search-wrap">
-                            <input type="text" placeholder="¿Buscar tours?" class="form-control search-field"><i class="flaticon-suntour-search search-icon"></i>
-                          </div>
-                        </form>
-                      </div>
-                      <div class="close-button"><span>Search</span></div>
-                    </div>
-                  </li>
-                </ul>
-              </li>
               <!-- End Search-->
             </ul>
           </div>
